e94c6e436cf489fa4612ad00d07f3963cbcc29c4
Zizheng Wu <me@zizhengwu.com>
2015-11-24 09:46:30 -0500

    deploy on heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,4 +75,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   # config.active_record.dump_schema_after_migration = false
+
+  config.location_dict = JSON.parse(File.read('vendor/assets/data/location.txt'))
 end
