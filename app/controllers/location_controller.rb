c9366c9e0ad616ab51ed622ea10186f6608fc99f
Zizheng Wu <me@zizhengwu.com>
2015-11-26 21:20:49 -0500

    unix_time in location is an optional parameter now

--- app/controllers/location_controller.rb
+++ app/controllers/location_controller.rb
@@ -4,11 +4,16 @@ require 'redis'
 class LocationController < ApplicationController
 
   def show
-    @location_id = params['location_id']
-    @unix_time = params['unix_time']
-
     redis = Redis.new
-    @photos = JSON.parse(redis.get("location_#{@location_id}_#{@unix_time}") || '[]')
+
+    @location_id = params['location_id']
+    if params.has_key?('unix_time')
+      @unix_time = params['unix_time']
+      @photos = JSON.parse(redis.get("location_#{@location_id}_#{@unix_time}") || '[]')
+    else
+      @photos = JSON.parse(redis.get("location_#{@location_id}") || '[]')
+    end
+
     respond_to do |format|
       format.html
       format.json { render json: @photos }
